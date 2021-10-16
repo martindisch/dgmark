971c132b5d8acfb287a2cb4af99a93940c150cfd
Martin Disch <martindisch@gmail.com>
2021-10-16 11:54:03 +0200

    Fix greedy text issue with better take_until1

--- src/text.rs
+++ src/text.rs
@@ -1,38 +1,36 @@
 use nom::{
-    branch::alt,
-    bytes::complete::{take_until1, take_while1},
     error::{ErrorKind, ParseError},
-    Err, FindSubstring, IResult, InputLength, InputTake,
+    lib::std::ops::RangeFrom,
+    Err, FindSubstring, IResult, InputLength, InputTake, Slice,
 };
 
 pub fn parse(input: &str) -> IResult<&str, &str> {
-    // TODO: if take_until1 fails (e.g. when tag is first thing encountered),
-    // take_while1 will eat everything. To prevent it, this parser needs to be
-    // the last one evaluated, which is not necessarily intuitive.
     take_until1_relaxed("[[")(input)
 }
 
+/// Custom implementation that doesn't error if the pattern is not found.
 fn take_until1_relaxed<T, Input, Error: ParseError<Input>>(
     tag: T,
 ) -> impl Fn(Input) -> IResult<Input, Input, Error>
 where
-    Input: InputTake + FindSubstring<T>,
+    Input:
+        Slice<RangeFrom<usize>> + InputLength + InputTake + FindSubstring<T>,
     T: InputLength + Clone,
 {
     move |i: Input| {
         let t = tag.clone();
-        let res = match i.find_substring(t) {
-            None => Err(Err::Error(Error::from_error_kind(
-                i,
-                ErrorKind::TakeUntil,
-            ))),
+        match i.find_substring(t) {
             Some(0) => Err(Err::Error(Error::from_error_kind(
                 i,
                 ErrorKind::TakeUntil,
             ))),
             Some(index) => Ok(i.take_split(index)),
-        };
-        res
+            None if i.input_len() > 0 => Ok((i.slice(i.input_len()..), i)),
+            None => Err(Err::Error(Error::from_error_kind(
+                i,
+                ErrorKind::TakeUntil,
+            ))),
+        }
     }
 }
 
