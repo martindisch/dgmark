b2cef1536f5c8308b8ee6e0c07746a5fa8711aa5
Martin Disch <martindisch@gmail.com>
2021-11-06 23:38:19 +0100

    Set up C# FFI example

---
+++ examples/DgMarkCSharp/DgMark.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+public static class DgMark
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct TextsDescriptor
+    {
+        public unsafe IntPtr* Texts;
+
+        public int Size;
+    }
+
+    [DllImport("../../target/release/libdgmark_ffi", CallingConvention = CallingConvention.Cdecl, EntryPoint = "texts")]
+    static extern TextsDescriptor TextsFfi(string input);
+
+    unsafe public static IEnumerable<string> Texts(string input)
+    {
+        var textsDescriptor = TextsFfi(input);
+
+        return Enumerable
+            .Range(0, textsDescriptor.Size)
+            .Select(i => Marshal.PtrToStringUTF8(textsDescriptor.Texts[i]));
+    }
+}
---
+++ examples/DgMarkCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DgMarkCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = @"Hi there! Let's see a productlist:
+[[productlist: 1|2|20]]
+
+That went well. How about another one?
+
+[[productlist:20]]
+
+We can do quotes too.
+
+[[quote:This is supposed to be the quote""And this is the source""]]
+
+Pretty cool, right?";
+
+            var texts = DgMark.Texts(input);
+
+            foreach (var text in texts)
+            {
+                Console.WriteLine($"- {text}");
+            }
+        }
+    }
+}
