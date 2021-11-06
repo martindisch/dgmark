517dfeb2e70011bab3f5a1d4fd9a9ccf4abfc4a0
Martin Disch <martindisch@gmail.com>
2021-11-06 23:20:10 +0100

    Return size of array together with pointer

--- dgmark-ffi/src/lib.rs
+++ dgmark-ffi/src/lib.rs
@@ -3,15 +3,22 @@ use std::{
     os::raw::c_char,
 };
 
-// TODO: check if extern "C" is necessary
+/// Struct holding both the string array pointer and its size.
+#[repr(C)]
+pub struct Texts {
+    pub texts: *const *mut c_char,
+    pub size: i32,
+}
+
+/// Parses markdown and returns the list of translatable texts.
 #[no_mangle]
-pub extern "C" fn texts(input: *const c_char) -> *const *mut c_char {
+pub extern "C" fn texts(input: *const c_char) -> Texts {
     let input = unsafe {
         assert!(!input.is_null());
         CStr::from_ptr(input).to_str().unwrap()
     };
 
-    let texts = match dgmark::parse(input) {
+    let texts_vec = match dgmark::parse(input) {
         Ok(("", elements)) => elements
             .into_iter()
             .flat_map(|e| e.texts())
@@ -19,8 +26,17 @@ pub extern "C" fn texts(input: *const c_char) -> *const *mut c_char {
             .collect(),
         _ => vec![],
     };
+    let texts_pointer = texts_vec.as_ptr();
 
-    texts.as_ptr()
+    let texts = Texts {
+        texts: texts_pointer,
+        size: texts_vec.len() as i32,
+    };
+
+    // Force leaking of the Vec, otherwise it will be freed
+    std::mem::forget(texts_vec);
+
+    texts
 }
 
 // TODO: need extra function to deallocate our returned Vec
