d41abd2e9c948242762e8374ea0e9cf3aef94dfe
Martin Disch <martindisch@gmail.com>
2021-11-09 20:33:18 +0100

    Use nice monadic functions

--- dgmark-wasm/src/lib.rs
+++ dgmark-wasm/src/lib.rs
@@ -8,14 +8,13 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 /// Parses markdown and returns the list of translatable texts.
 #[wasm_bindgen]
 pub fn texts(input: &str) -> Result<Array, JsValue> {
-    match dgmark::texts(input) {
-        Ok(texts) => {
+    dgmark::texts(input)
+        .and_then(|texts| {
             let array = Array::new();
             texts.into_iter().map(JsValue::from_str).for_each(|v| {
                 array.push(&v);
             });
             Ok(array)
-        }
-        Err(e) => Err(JsValue::from_str(e)),
-    }
+        })
+        .or_else(|e| Err(JsValue::from_str(e)))
 }
