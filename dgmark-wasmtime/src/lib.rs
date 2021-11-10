770417e6a4db4548ae6cb4ae72ec936ac17e44af
Martin Disch <martindisch@gmail.com>
2021-11-10 20:55:38 +0100

    Rearrange functions

--- dgmark-wasmtime/src/lib.rs
+++ dgmark-wasmtime/src/lib.rs
@@ -3,22 +3,6 @@ use std::{
     slice, str,
 };
 
-/// Allocates some memory in this address space.
-#[no_mangle]
-pub unsafe fn __alloc(len: usize) -> *mut u8 {
-    let align = std::mem::align_of::<usize>();
-    let layout = Layout::from_size_align_unchecked(len, align);
-    alloc(layout)
-}
-
-/// Frees some memory in this address space.
-#[no_mangle]
-pub unsafe fn __dealloc(bytes: *mut u8, len: usize) {
-    let align = std::mem::align_of::<usize>();
-    let layout = Layout::from_size_align_unchecked(len, align);
-    dealloc(bytes, layout);
-}
-
 /// Struct holding a starting address and length.
 #[repr(C)]
 pub struct Array {
@@ -57,4 +41,20 @@ pub fn texts(input: *const u8, len: usize) -> *const Array {
     std::mem::forget(texts);
 
     texts_array
+}
+
+/// Allocates some memory in this address space.
+#[no_mangle]
+pub unsafe fn __alloc(len: usize) -> *mut u8 {
+    let align = std::mem::align_of::<usize>();
+    let layout = Layout::from_size_align_unchecked(len, align);
+    alloc(layout)
+}
+
+/// Frees some memory in this address space.
+#[no_mangle]
+pub unsafe fn __dealloc(bytes: *mut u8, len: usize) {
+    let align = std::mem::align_of::<usize>();
+    let layout = Layout::from_size_align_unchecked(len, align);
+    dealloc(bytes, layout);
 }
