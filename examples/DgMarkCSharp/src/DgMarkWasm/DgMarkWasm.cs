6992aeaa46507cc9a626d0c69ea29771881feda4
Martin Disch <martindisch@gmail.com>
2021-11-10 20:07:19 +0100

    Refactor WASM example for easier benchmarking

---
+++ examples/DgMarkCSharp/src/DgMarkWasm/DgMarkWasm.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wasmtime;
+
+namespace DgMarkWasmExample
+{
+    public class DgMarkWasm : IDisposable
+    {
+        private Engine engine;
+        private Module module;
+        private Linker linker;
+
+        public DgMarkWasm()
+        {
+            engine = new Engine();
+            module = Module.FromFile(
+                engine,
+                "../../target/wasm32-unknown-unknown/release/dgmark_wasmtime.wasm");
+            linker = new Linker(engine);
+        }
+
+        public IEnumerable<string> Texts(string input)
+        {
+            using var store = new Store(engine);
+            var instance = linker.Instantiate(store, module);
+            var memory = instance.GetMemory(store, "memory");
+
+            var alloc = instance.GetFunction(store, "__alloc");
+            var dealloc = instance.GetFunction(store, "__dealloc");
+            var texts = instance.GetFunction(store, "texts");
+
+            // Put input into WASM memory
+            var utf8Input = Encoding.UTF8.GetBytes(input);
+            var offset = (int)alloc.Invoke(store, utf8Input.Length);
+            var allocatedSlice = memory.GetSpan(store).Slice(offset);
+            utf8Input.AsSpan<byte>().CopyTo(allocatedSlice);
+
+            var arrayDescriptorOffset = (int)texts.Invoke(store, offset, utf8Input.Length);
+
+            return ExtractTexts(memory, store, arrayDescriptorOffset);
+        }
+
+        static IEnumerable<string> ExtractTexts(Memory memory, Store store, int arrayDescriptorOffset)
+        {
+            var arrayOffset = memory.ReadInt32(store, arrayDescriptorOffset);
+            var arrayLength = memory.ReadInt32(store, arrayDescriptorOffset + 4);
+
+            return Enumerable
+                .Range(0, arrayLength)
+                .Select(i =>
+                {
+                    var currentArrayDescriptorOffset = memory.ReadInt32(store, arrayOffset + i * 4);
+
+                    var currentStringOffset = memory.ReadInt32(store, currentArrayDescriptorOffset);
+                    var currentStringLength = memory.ReadInt32(store, currentArrayDescriptorOffset + 4);
+
+                    return memory.ReadString(store, currentStringOffset, currentStringLength);
+                });
+        }
+
+        public void Dispose()
+        {
+            engine.Dispose();
+            module.Dispose();
+            linker.Dispose();
+        }
+    }
+}
--- examples/DgMarkCSharp/src/DgMarkWasm/Program.cs
+++ examples/DgMarkCSharp/src/DgMarkWasm/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Wasmtime;
 
-namespace DgMarkWasm
+namespace DgMarkWasmExample
 {
     class Program
     {
@@ -23,50 +19,13 @@ We can do quotes too.
 
 Pretty cool, right?";
 
-            using var engine = new Engine();
-            using var linker = new Linker(engine);
-            using var store = new Store(engine);
-            using var module = Module.FromFile(
-                engine,
-                "../../target/wasm32-unknown-unknown/release/dgmark_wasmtime.wasm");
-            var instance = linker.Instantiate(store, module);
-            var memory = instance.GetMemory(store, "memory");
+            using var dgMarkWasm = new DgMarkWasm();
 
-            var alloc = instance.GetFunction(store, "__alloc");
-            var dealloc = instance.GetFunction(store, "__dealloc");
-            var texts = instance.GetFunction(store, "texts");
-
-            // Put input into WASM memory
-            var utf8Input = Encoding.UTF8.GetBytes(input);
-            var offset = (int)alloc.Invoke(store, utf8Input.Length);
-            var allocatedSlice = memory.GetSpan(store).Slice(offset);
-            utf8Input.AsSpan<byte>().CopyTo(allocatedSlice);
-
-            var arrayDescriptorOffset = (int)texts.Invoke(store, offset, utf8Input.Length);
-
-            var extractedTexts = ExtractTexts(memory, store, arrayDescriptorOffset);
-            foreach (var text in extractedTexts)
+            var texts = dgMarkWasm.Texts(input);
+            foreach (var text in texts)
             {
                 Console.WriteLine($"- {text}");
             }
-        }
-
-        static IEnumerable<string> ExtractTexts(Memory memory, Store store, int arrayDescriptorOffset)
-        {
-            var arrayOffset = memory.ReadInt32(store, arrayDescriptorOffset);
-            var arrayLength = memory.ReadInt32(store, arrayDescriptorOffset + 4);
-
-            return Enumerable
-                .Range(0, arrayLength)
-                .Select(i =>
-                {
-                    var currentArrayDescriptorOffset = memory.ReadInt32(store, arrayOffset + i * 4);
-
-                    var currentStringOffset = memory.ReadInt32(store, currentArrayDescriptorOffset);
-                    var currentStringLength = memory.ReadInt32(store, currentArrayDescriptorOffset + 4);
-
-                    return memory.ReadString(store, currentStringOffset, currentStringLength);
-                });
         }
     }
 }
