c1e254bf572699120a6ae1bc13126ac5a90b6bb1
Martin Disch <martindisch@gmail.com>
2021-11-25 20:15:55 +0100

    Benchmark universal example

--- examples/DgMarkCSharp/src/Bench/Bencher.cs
+++ examples/DgMarkCSharp/src/Bench/Bencher.cs
@@ -3,6 +3,7 @@ using System.IO;
 using BenchmarkDotNet.Attributes;
 using DgMarkFfiExample;
 using DgMarkWasmExample;
+using DgMarkUniversalWasmExample;
 
 namespace Bench
 {
@@ -11,11 +12,13 @@ namespace Bench
         const string InputPath = "/home/martin/Projects/dgmark/dgmark/benches/inputs/text_with_everything.md";
 
         private readonly DgMarkWasm dgMarkWasm;
+        private readonly DgMarkUniversalWasm dgMarkUniversalWasm;
         private readonly string input;
 
         public Bencher()
         {
             dgMarkWasm = new DgMarkWasm();
+            dgMarkUniversalWasm = new DgMarkUniversalWasm();
             input = File.ReadAllText(InputPath);
         }
 
@@ -24,5 +27,8 @@ namespace Bench
 
         [Benchmark]
         public IReadOnlyCollection<string> Wasm() => dgMarkWasm.Texts(input);
+
+        [Benchmark]
+        public IReadOnlyCollection<string> WasmUniversal() => dgMarkUniversalWasm.Texts(input);
     }
 }
